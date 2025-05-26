81e69f9539135f0f16914dab051df359cae95e52
Kyan Van den Eynde <66461508+kyanvde@users.noreply.github.com>
2025-05-26 18:52:15 +0200

    Add solution for 217.Contains-Duplicate (easy)

---
+++ easy/217.Contains-Duplicate.cpp
@@ -0,0 +1,8 @@
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        return unordered_set<int>(nums.begin(), nums.end()).size() < nums.size();
+    }
+};
